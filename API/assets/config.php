6ca90b7f3c56f2cff2c4d47a452b3c585e3a95b8
Luca Dovichi <lucadovichi@gmail.com>
2022-02-05 22:47:40 -0800

    Fixing inline variable formatting

--- API/assets/config.php
+++ API/assets/config.php
@@ -8,7 +8,7 @@ try {
   $DB_NAME = "";
   $DB_USERNAME = "";
   $DB_PASSWORD = "";
-  $dbh = new PDO("mysql:host=${DB_HOST};dbname={$DB_NAME}", "${DB_USERNAME}", "${DB_PASSWORD}");
+  $dbh = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}", "{$DB_USERNAME}", "{$DB_PASSWORD}");
 } catch (PDOException $e) {
   errorOut("DB connection failed to initialize");
 }
